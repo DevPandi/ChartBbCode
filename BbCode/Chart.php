da1860790b3bf3d4a4cb5dd0a1adafc58530df5c
DevPandi <pandi@posteo.de>
2024-01-30 15:13:46 +0100

    Second version

--- BbCode/Chart.php
+++ BbCode/Chart.php
@@ -2,106 +2,150 @@
 
 namespace DevPandi\ChartBbCode\BbCode;
 
+use XF\Template\Templater;
+
 class Chart
 {
+    protected static bool $escape = false;
+    protected static ?int $minValue = null;
+    protected static ?int $maxValue = null;
     public static function renderTagBar($tagChildren, $tagOption, $tag, array $options, \XF\BbCode\Renderer\AbstractRenderer $renderer)
     {
-        $chartID = substr(sha1(rand()), 0, 10);
-        $rawString = $tagChildren[0];
-        if (!mb_strlen($rawString > 0)) {
-            return '';
+        // reset static value
+        static::$minValue = static::$maxValue = null;
+
+        // parse options and tag
+        $optionsString = static::unifyLines($tagOption);
+        $chartString = static::unifyLines($tagChildren[0]);
+        $chartOptions = static::parseOptions($optionsString, $renderer->getTemplater());
+        $chartElements = static::parseChart($chartString, $renderer->getTemplater());
+
+        // no options for min and max.
+        if (!isset($chartOptions['startAt'])) {
+            $chartOptions['startAt'] = static::findNumber();
         }
-        $title = '';
-        $parts = array();
-        $partCount = 1;
-        $elements = array();
-        $maxValue = 0;
-
-        $rawString = str_replace(["\r\n", "\r"], "\n", $rawString);
-        $rawElements = explode("\n", $rawString);
-        foreach ($rawElements as $rawElement) {
-            if (!empty(trim($rawElement))) {
-                list($tag, $data) = explode(":", $rawElement);
-                $rawData[trim($tag)] = trim($data);
-            }
+        if (!isset($chartOptions['endAt'])) {
+            $chartOptions['endAt'] = static::findNumber(false);
         }
-
-        $title = $rawData['title'] ?? 'Chart';
-
-        if (isset($rawData['parts'])) {
-            $parts = explode(',', $rawData['parts']);
-            $partCount = count($parts);
-        }
-
-        if (!isset($rawData['elements'])) {
-            return 'Missing elemnts for chart';
-        } else {
-            $rawElements = explode(',', $rawData['elements']);
-
-            foreach ($rawElements as $rawElement) {
-                $elements[] = ['name' => trim($rawElement), 'color' => '', 'data' => []];
-            }
-        }
-
-        if (!isset($rawData['data'])) {
-            return 'Missing data';
-        } else {
-            if ($partCount > 1) {
-                preg_match_all('#\[[^\]]+\]#', $rawData['data'], $dataStrings);
-                foreach ($dataStrings[0] as $index => $rawValue) {
-                    $rawValue = str_replace(['[', ']'], '', $rawValue);
-                    $values = explode(',', $rawValue);
-                    foreach ($values as $value) {
-                        $maxValue = ($value > $maxValue) ? $value : $maxValue;
-                        $elements[$index]['data'][] = $value;
-                    }
-                }
-            } else {
-                $rawValue = explode(',', $rawData['data']);
-                foreach ($rawValue as $index => $value) {
-                    $elements[$index]['data'][0] = $value;
-
-                    $maxValue = ($value > $maxValue) ? $value : $maxValue;
-                }
-            }
-        }
-
-        if (isset($rawData['color'])) {
-            if (
-                preg_match_all(
-                    '#rgb\([0-9]{1,3},[0-9]{1,3},[0-9]{1,3}\)|rgba\([0-9]{1,3},[0-9]{1,3},[0-9]{1,3},[0,1].[0-9]\)#',
-                    $rawData['color'],
-                    $colors
-                )
-            ) {
-                foreach ($colors[0] as $index => $color) {
-                    $elements[$index]['color'] = trim($color);
-                }
-            } else {
-                $colors = explode(',', $rawData['color']);
-                foreach ($colors as $index => $color) {
-                    $elements[$index]['color'] = trim($color);
-                }
-            }
-        }
-
-        $titleY = $rawData['titleY'] ?? '';
-        $startAt = $rawData['startAt'] ?? 0;
-        $endAt = $rawData['endAt'] ?? static::maxValue($maxValue);
 
         return $renderer->getTemplater()->renderTemplate('public:devpandi_bb_code_tag_chartbar', [
-            'chartID' => $chartID,
-            'parts' => $parts,
-            'elements' => $elements,
-            'titleY' => $titleY,
-            'startAt' => $startAt,
-            'endAt' => $endAt,
-            'title' => $title,
+            'chartID' => substr(sha1(rand()), 0, 15),
+            'chartOptions' => $chartOptions,
+            'chartElements' => $chartElements,
         ]);
     }
 
-    protected static function maxValue($maxValue)
+    protected static function parseOptions(string $optionsString, Templater $templater): array
     {
-        return ($maxValue < 100) ? $maxValue + (5 - ($maxValue % 5)) : $maxValue + (10 - ($maxValue % 10));
+        $options = [];
+        if (!empty($optionsString)) {
+            $rawOptions = explode(';', $optionsString);
+
+            foreach ($rawOptions as $optionString) {
+                if (strpos($optionString, ':')) {
+                    $rawOption = explode(':', $optionString);
+                    $tag = static::trim($rawOption[0], $templater) ?? '';
+                    $value = static::trim($rawOption[1], $templater) ?? '';
+
+                    if ($tag !== '' && $value !== '') {
+                        switch ($tag) {
+                            case 'title':
+                            case 'startAt':
+                            case 'endAt':
+                                $options[$tag] = $value;
+                                break;
+                            case 'y':
+                                if (str_starts_with($value, '#')) {
+                                    $options[$tag] = ['tick' => true, 'str' => str_replace('#', '', $value), 'start' => true];
+                                } elseif (str_ends_with($value, '#')) {
+                                    $options[$tag] = ['tick' => true, 'str' => str_replace('#', '', $value), 'start' => false];
+                                } else {
+                                    $options[$tag] = ['tick' => false, 'str' => $value];
+                                }
+                                break;
+                            case 'height':
+                            case 'width':
+                                $options[$tag] = (int) $value;
+                        }
+                    }
+                }
+            }
+        }
+
+        return $options;
+    }
+
+    protected static function parseChart(string $chartString, Templater $templater): array
+    {
+        $elements = [];
+        $elementName = '';
+        $x = 1;
+        $lines = explode("\n", $chartString);
+        foreach ($lines as $lineNo => $line) {
+            if (str_starts_with($line, 'x:')) {
+                $elements['x'] = explode(';', substr($line, 2));
+                $x = count($elements['x']);
+                continue;
+            }
+
+            $rawElement = explode(';', $line);
+            foreach ($rawElement as $index => $value) {
+                $value = static::trim($value, $templater);
+                if ($index === 0) {
+                    $elements[$value] = [];
+                    $elementName = $value;
+                    continue;
+                }
+
+                if (empty($elementName)) {
+                    break;
+                }
+
+                if (str_starts_with($value, 'color:')) {
+                    $elements[$elementName]['color'] = static::trim(str_replace('color:', '', $value), $templater);
+                    continue;
+                }
+
+                if (str_starts_with($value, 'border:')) {
+                    $elements[$elementName]['border'] = static::trim(str_replace('border:', '', $value), $templater);
+                    continue;
+                }
+
+                static::$maxValue = (static::$maxValue === null || static::$maxValue < (int) $value) ? (int) $value : static::$maxValue;
+                static::$minValue = (static::$minValue === null || static::$minValue > (int) $value) ? (int) $value : static::$minValue;
+                $elements[$elementName]['data'][] = $value;
+            }
+        }
+
+        return $elements;
+    }
+
+    protected static function unifyLines(string $string): string
+    {
+        return str_replace(["\r\n", "\r"], "\n", $string);
+    }
+
+    protected static function trim(string $string, Templater $templater): string
+    {
+        return $templater->fnTrim($templater, static::$escape, $string);
+    }
+
+    protected static function findNumber(bool $start = true): int
+    {
+        if ($start) {
+            return static::$minValue - (static::$minValue % static::findModulo(static::$maxValue));
+        }
+
+        return static::$maxValue + (static::$maxValue % static::findModulo(static::$maxValue));
+    }
+
+    protected static function findModulo(int $number): int
+    {
+        return match (true) {
+            $number <= 50 => 5,
+            $number <= 100 => 10,
+            $number <= 1000 => 100,
+            $number >= 10000 => 1000,
+        };
     }
 }
---
+++ readme.md
@@ -0,0 +1,10 @@
+# [DevPandi] Chart BB Code
+Example:
+```
+[chartbar=title:Some Title; y:# fps;]x:avg. fps;P1 fps
+RTX 4090;color:rgba(0,191,91,0.2);border:rgb(0,191,91);55;50
+7900 XTX;color:rgba(191,0,91,0.2);border:rgb(191,0,91);53;47[/chartbar]
+```
+
+Parse to:
+![Parsed example of bb code.](img/result.png)
