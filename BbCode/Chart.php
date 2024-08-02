2972ac86f2e2d84652da388cfa0b34caa1c4f96e
DevPandi <pandi@posteo.de>
2024-08-02 17:32:19 +0200

    Prepare "line" bb code.

--- BbCode/Chart.php
+++ BbCode/Chart.php
@@ -2,17 +2,50 @@
 
 namespace DevPandi\ChartBbCode\BbCode;
 
-use XF\Template\Templater;
+use \XF\BbCode\Renderer\AbstractRenderer;
 
 class Chart
 {
+    protected static Abstractrenderer $renderer;
     protected static bool $escape = false;
     protected static ?int $minValue = null;
     protected static ?int $maxValue = null;
+    protected static array $supportedOptions = [
+        'bar' => [
+            'title',
+            'min',
+            'max',
+            'y',
+            'height',
+            'width',
+            'useX',
+        ],
+        'line' => [
+            'title',
+            'min',
+            'max',
+            'y',
+            'x',
+            'height',
+            'width'
+        ]
+    ];
 
-    public static function renderTagBar($tagChildren, $tagOption, $tag, array $options, \XF\BbCode\Renderer\AbstractRenderer $renderer)
+    public static function renderBarTag($tagChildren, $tagOption, $tag, array $options, \XF\BbCode\Renderer\AbstractRenderer $renderer)
     {
-        // reset static value
+        static::$renderer = $renderer;
+        return static::renderTag($tagChildren, $tagOption, $tag, $options, 'bar');
+    }
+
+    public static function renderLineTag($tagChildren, $tagOption, $tag, array $options, \XF\BbCode\Renderer\AbstractRenderer $renderer)
+    {
+        static::$renderer = $renderer;
+        return static::renderTag($tagChildren, $tagOption, $tag, $options, 'line');
+    }
+
+    protected static function renderTag($tagChildren, $tagOption, $tag, array $options, $type)
+    {
+        // set defaults
         static::$minValue = static::$maxValue = null;
 
         // prepare chart string
@@ -21,11 +54,11 @@ class Chart
 
         //Build Chart Data
         $chartData = array_merge(
-            static::parseOptions($optionsString, $renderer->getTemplater()),
-            static::parseChart($chartString, $renderer->getTemplater())
+            static::parseOptions($optionsString),
+            static::parseChart($chartString)
         );
         $chartData['id'] = substr(sha1(rand()), 0, 15);
-        $chartData['type'] = 'bar';
+        $chartData['type'] = $type;
 
         // no options for min and max.
         if (!isset($chartOptions['min'])) {
@@ -35,12 +68,12 @@ class Chart
             $chartData['max'] = static::findNumber(false);
         }
 
-        return $renderer->getTemplater()->renderTemplate('public:devpandi_bb_code_tag_chartbar', [
+        return static::$renderer->getTemplater()->renderTemplate('public:devpandi_bb_code_tag_chart', [
             'chartData' => $chartData,
         ]);
     }
 
-    protected static function parseOptions(string $optionsString, Templater $templater): array
+    protected static function parseOptions(string $optionsString): array
     {
         $options = [];
         if (!empty($optionsString)) {
@@ -49,8 +82,8 @@ class Chart
             foreach ($rawOptions as $optionString) {
                 if (strpos($optionString, ':')) {
                     $rawOption = explode(':', $optionString);
-                    $tag = static::trim($rawOption[0], $templater) ?? '';
-                    $value = static::trim($rawOption[1], $templater) ?? '';
+                    $tag = static::trim($rawOption[0]) ?? '';
+                    $value = static::trim($rawOption[1]) ?? '';
 
                     if ($tag !== '' && $value !== '') {
                         switch ($tag) {
@@ -61,12 +94,15 @@ class Chart
                                 break;
                             case 'y':
                                 if (str_starts_with($value, '#')) {
-                                    $options[$tag] = ['tick' => true, 'str' => str_replace('#', '', $value), 'start' => true];
+                                    $options['y_axis'] = ['tick' => true, 'str' => str_replace('#', '', $value), 'start' => true];
                                 } elseif (str_ends_with($value, '#')) {
-                                    $options[$tag] = ['tick' => true, 'str' => str_replace('#', '', $value), 'start' => false];
+                                    $options['y_axis'] = ['tick' => true, 'str' => str_replace('#', '', $value), 'start' => false];
                                 } else {
-                                    $options[$tag] = ['tick' => false, 'str' => $value];
+                                    $options['y_axis'] = ['tick' => false, 'str' => $value];
                                 }
+                                break;
+                            case 'x':
+                                $options['x_axis'] = $value;
                                 break;
                             case 'height':
                             case 'width':
@@ -84,7 +120,7 @@ class Chart
         return $options;
     }
 
-    protected static function parseChart(string $chartString, Templater $templater): array
+    protected static function parseChart(string $chartString): array
     {
         $labels = [];
         $elements = [];
@@ -98,7 +134,7 @@ class Chart
             $rawElement = explode(';', $line);
             $element = [];
             foreach ($rawElement as $index => $value) {
-                $value = static::trim($value, $templater);
+                $value = static::trim($value);
                 if ($index === 0) {
                     $element = [];
                     $element['name'] = $value;
@@ -110,12 +146,22 @@ class Chart
                 }
 
                 if (str_starts_with($value, 'color:')) {
-                    $element['color'] = static::trim(str_replace('color:', '', $value), $templater);
+                    $element['color'] = static::trim(str_replace('color:', '', $value));
                     continue;
                 }
 
                 if (str_starts_with($value, 'border:')) {
-                    $element['border'] = static::trim(str_replace('border:', '', $value), $templater);
+                    $element['border'] = static::trim(str_replace('border:', '', $value));
+                    continue;
+                }
+
+                if ($value == 'dashed') {
+                    $element['dashed'] = true;
+                    continue;
+                }
+
+                if (str_starts_with($value, 'point:')) {
+                    $element['point'] = static::parsePoint(str_replace('point:', '', $value));
                     continue;
                 }
 
@@ -133,14 +179,38 @@ class Chart
         return ['x' => $labels, 'y' => $elements];
     }
 
+    protected static function parsePoint(string $value): array
+    {
+        list($point, $size) = explode(',', $value);
+        echo $size;
+
+        $point = match ($point) {
+            'cross' => 'cross',
+            'crossRot' => 'crossRot',
+            'dash' => 'dash',
+            'line' => 'line',
+            'rect' => 'rect',
+            'rectRounded' => 'rectRounded',
+            'rectRot' => 'rectRot',
+            'star' => 'star',
+            'triangle' => 'triangle',
+            default => 'circle',
+        };
+
+        $size = (int) $size;
+        $size = ($size < 10) ? 10 : $size;
+
+        return ['style' => $point, 'size' => $size];
+    }
+
     protected static function unifyLines(string $string): string
     {
         return str_replace(["\r\n", "\r"], "\n", $string);
     }
 
-    protected static function trim(string $string, Templater $templater): string
+    protected static function trim(string $string): string
     {
-        return $templater->fnTrim($templater, static::$escape, $string);
+        return static::$renderer->getTemplater()->fnTrim(static::$renderer->getTemplater(), static::$escape, $string);
     }
 
     protected static function findNumber(bool $start = true): int
--- js/DevPandi/ChartBbCode.js
+++ js/DevPandi/ChartBbCode.js
@@ -1,86 +1,175 @@
 ((window, document) => {
+    'use strict'
+    XF.DevPandi = XF.DevPandi || {};
 
-
-    var charts = document.getElementsByName('chartBbcodeData');
-    for (i = 0; i < charts.length; i++) {
-        var chart = JSON.parse(charts[i].textContent);
-        console.log(chart);
-        // create chart data
-        var chartData = {};
-        if (chart.x) {
-            chartData.labels = chart.x;
+    class ChartBbCode {
+        constructor() {
+            this.charts = [];
         }
-        var datasets = [];
-        for (j = 0; j < chart.y.elements.length; j++) {
-            var element =  chart.y.elements[j];
-            var dataset = {};
-            dataset.label = element.name;
-            if (element.color) {
-                dataset.backgroundColor = [element.color];
+
+        renderCharts() {
+            var rawCharts = document.getElementsByClassName('chartBbcodeData');
+            for (var i = 0; i < rawCharts.length; i++) {
+                this.renderChart(rawCharts[i]);
             }
-            if (element.border) {
-                dataset.borderColor = [element.border];
+        }
+
+        renderChart(rawChart) {
+            var chartData = JSON.parse(rawChart.textContent);
+            var id = chartData.id;
+
+            if (this.charts[id]) {
+                return;
             }
 
-            dataset.data = element.data;
+            this.charts[id] = {data: {}, options: {}, type: chartData.type};
+            this.charts[id].options = this.readOptions(chartData, this.charts[id].type);
+            this.charts[id].data = this.readData(chartData, this.charts[id].type);
 
-            datasets[j] = dataset;
-        }
-        chartData.datasets = datasets;
 
-        // create options
-        var chartOptions = {};
-        if (chart.useX) {
-            chartOptions.indexAxis = 'y';
-        }
-        chartOptions.plugins = {};
-        chartOptions.plugins.legend = {};
-        chartOptions.plugins.legend.position = 'bottom';
-
-        if (chart.title) {
-            chartOptions.plugins.title = {};
-            chartOptions.plugins.title.display = true;
-            chartOptions.plugins.title.text = chart.title;
+            var chartBox = document.getElementById('chart-' + id);
+            new Chart(chartBox, {type: this.charts[id].type, data: this.charts[id].data, options: this.charts[id].options });
         }
 
-        chartOptions.responsive = true;
-        chartOptions.elements = {};
-        chartOptions.elements.bar = {};
-        chartOptions.elements.bar.borderWidth = 2
+        readOptions(chartData, type) {
+            var options = {}
+            options.plugins = {};
+            options.plugins.legend = {};
+            options.plugins.legend.position = 'bottom';
+            options.responsive = true;
+            options.elements = {};
+            options.elements.bar = {};
+            options.elements.bar.borderWidth = 2
 
-        var axis = {};
-        if (chart.startAt > 0) {
-            axis.min = chart.startAt;
-        } else {
-            axis.beginAtZero = true;
-        }
-
-        if (chart.endAt) {
-            axis.max = chartOptions.endAt;
-        }
-
-        if (chart.y.tick == false && chart.y.str) {
-            axis.title = {}
-            axis.title.display = true;
-            axis.title.text = chart.y.str;
-        } else {
-            axis.ticks = {};
-            if (chart.y.start) {
-                axis.ticks.callback = function(value, index, ticks) { return value + chart.y.str; };
+            options.scales = {};
+            if (chartData.useX) {
+                options.indexAxis = 'y';
+                options.scales.x = this.readOptionsAxis(chartData);
             } else {
-                axis.ticks.callback = function(value, index, ticks) { return  chart.y.str + value; };
+                options.scales.y = this.readOptionsAxis(chartData);
+                if (type == 'line' && chartData.x_axis) {
+                    options.scales.x = { };
+                    options.scales.x.display = true;
+                    options.scales.x.title = { display: true, text: chartData.x_axis };
+                }
             }
+
+            if (chartData.title) {
+                options.plugins.title = {};
+                options.plugins.title.display = true;
+                options.plugins.title.text = chartData.title;
+            }
+
+            return options;
         }
 
-        chartOptions.scales = {};
-        if (chart.useX) {
-            chartOptions.scales.x = axis;
-        } else {
-            chartOptions.scales.y = axis;
+        readOptionsAxis(chartData, type) {
+            var axis = {};
+            if (chartData.min > 0) {
+                axis.min = chartData.min;
+            } else {
+                axis.beginAtZero = true;
+            }
+            if (chartData.end) {
+                axis.max = chartData.max;
+            }
+
+            if (!chartData.y_axis.tick && chartData.y_axis.str) {
+                axis.title = { display: true, text: chartData.y_axis.str };
+            } else {
+                axis.ticks = {};
+                if (chartData.y_axis.start) {
+                    axis.ticks.callback = function (value, index, ticks) {
+                        return value + chartData.y_axis.str;
+                    }
+                }
+                else {
+                    axis.ticks.callback = function (value, index, ticks) {
+                        return chartData.y_axis.str + value;
+                    }
+                }
+            }
+
+            return axis;
         }
 
-        chartBox = document.getElementById('chart-' + chart.id);
-        new Chart(chartBox, {type: 'bar', data: chartData, options: chartOptions});
+        readData(chartData, type) {
+            var data = { labels: [], datasets: [] };
+            var i = 0;
+
+            // labes for x or y axis
+            if (chartData.x) {
+                data.labels = chartData.x;
+            }
+
+            chartData.y.forEach(function (element) {
+                var dataset = {};
+                dataset.label = element.name;
+                if (element.color) {
+                    dataset.backgroundColor = [element.color];
+                }
+                if (element.border) {
+                    dataset.borderColor = [element.border];
+                }
+
+                if (type == 'line' && element.dashed) {
+                    dataset.borderDash = [5,5];
+                }
+
+                if (type == 'line' && element.point) {
+                    dataset.pointStyle = element.point.style,
+                    dataset.pointRadius = element.point.size;
+                    dataset.pointHoverRadius = element.point.size + 5;
+                }
+                dataset.data = element.data
+
+                data.datasets[i] = dataset;
+                i++;
+            })
+
+            return data;
+        }
     }
 
+    XF.DevPandi.ChartBbCode = new ChartBbCode();
+    XF.DevPandi.ChartBbCode.renderCharts();
+
+    XF.Element.extend("quick-reply", {
+        __backup: {
+            "afterSubmit": "_afterSubmitExtension"
+        },
+
+        afterSubmit: function(e, data)
+        {
+            this._afterSubmitExtension(e, data);
+
+            XF.DevPandi.ChartBbCode.renderCharts();
+        }
+    })
+
+    XF.Element.extend('quick-edit', {
+        __backup: {
+            "editSubmit": "_editSubmit"
+        },
+
+        editSubmit: function(e)
+        {
+            this._editSubmit(e);
+
+            XF.DevPandi.ChartBbCode.renderCharts();
+        }
+    })
+
+    XF.Element.extend('message-loader', {
+        __backup: {
+            "loaded": "_loaded"
+        },
+
+        loaded: function(data)
+        {
+            this._loaded(data);
+
+            XF.DevPandi.ChartBbCode.renderCharts();
+        }
+    })
 })(window, document)
